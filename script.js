ececa5aa7d39ba215d2396c4a13b38c0e3b0c08a
dilmurodivcc <zufarovamohire32.gmail.com>
2024-11-18 09:43:35 +0500

    Mobile And Menu

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 let currentIndex = 0;
-const slides = document.querySelectorAll('.carousel-slide img');
+const slides = document.querySelectorAll(".carousel-slide img");
 const totalSlides = slides.length;
-const indicators = document.querySelectorAll('.indicator');
+const indicators = document.querySelectorAll(".indicator");
 
 const slidePattern = [0, 1, 2, 1, 0, 1, 2, 1, 0];
 let patternIndex = 0;
 
 function updateIndicators() {
-    indicators.forEach((indicator, index) => {
-        if (index === slidePattern[patternIndex]) {
-            indicator.classList.add('active');
-        } else {
-            indicator.classList.remove('active');
-        }
-    });
+  indicators.forEach((indicator, index) => {
+    if (index === slidePattern[patternIndex]) {
+      indicator.classList.add("active");
+    } else {
+      indicator.classList.remove("active");
+    }
+  });
 }
 
 function showSlide(index) {
-    const offset = -index * 768;
-    document.querySelector('.carousel-slide').style.transform = `translateX(${offset}px)`;
-    updateIndicators();
+  const offset = -index * 768;
+  document.querySelector(
+    ".carousel-slide"
+  ).style.transform = `translateX(${offset}px)`;
+  updateIndicators();
 }
 
 function nextSlide() {
-    patternIndex = (patternIndex + 1) % slidePattern.length;
-    currentIndex = slidePattern[patternIndex];
-    showSlide(currentIndex);
+  patternIndex = (patternIndex + 1) % slidePattern.length;
+  currentIndex = slidePattern[patternIndex];
+  showSlide(currentIndex);
 }
 
-setInterval(nextSlide, 2500); 
-updateIndicators(); 
+setInterval(nextSlide, 2500);
+updateIndicators();
+
+const menu = document.querySelector(".menu");
+const menuMobile = document.querySelector(".menu-mobile");
+const close = document.querySelector(".close");
+
+menu.addEventListener("click", () => {
+  menuMobile.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+  menuMobile.classList.remove("active");
+  document.body.style.overflowY = "auto";
+});
